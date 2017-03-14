d8d3e8fe91ec9f0edd26a96b234bb5e80bc322fb
Dirk von Grünigen <dirk@vongruenigen.com>
2017-03-15 00:49:51 +0100

    use aconstant for the filename and only download the dataset if necessary

--- setup.sh
+++ setup.sh
@@ -2,23 +2,24 @@
 
 echo 'Downloading dataset...'
 
-#download dataset first,
-wget http://opus.lingfil.uu.se/download.php?f=OpenSubtitles/en.tar.gz
+OUT_NAME='opensubtitles.tar.gz'
+
+if [ ! -f $OUT_NAME ]; then
+  # download dataset if necessary
+  wget http://opus.lingfil.uu.se/download.php?f=OpenSubtitles/en.tar.gz -O $OUT_NAME
+fi
 
 echo 'Unzipping dataset...'
 
-#Making assumption that user hasn't put any other tar files in folder
-#Two tar ball extractions because during testing it downloaded as .gz once?
-tar -xvf *.tar
-tar -xvf *.gz
+tar -zxvf $OUT_NAME
 
 echo 'Extracting dataset...'
 
-#extract all filesin sub-directories
+# extract all files in sub-directories
 find . -name '*.gz' -exec gunzip '{}' \;
 
 echo 'Running python preprocessor...'
 
-#run python pre-processor
+# run python pre-processor
 python opensubtitleparser.py
 
